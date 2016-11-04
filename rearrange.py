dfd20190440856d741f4d370bd4d0383f5c8faa5
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-04 14:09:30 -0700

    Added two practice scripts for shuffling given inputs and then string reversing inputs

---
+++ rearrange.py
@@ -0,0 +1,27 @@
+"""
+   The intention of this script is to take user inputs and then shuffle
+   up the order the inputs and print the shuffled user inputs
+
+   For Example:
+   python rearrange.py Hello My Name Is Alex
+
+   Will print any random arangement of Hello, My, Name, Is, and Alex
+   Possible to print in the same order as inputed.
+"""
+import sys
+import random
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+original_arg_list = sys.argv
+random_order_list = []
+
+# Start the range a index 1 not 0 because the first argv is the command input
+# and not relevant to the user inputs
+for i in range(1, len(sys.argv)):
+    random_num = random.randint(1, len(original_arg_list)-1)
+    random_order_list.append(original_arg_list[random_num])
+    original_arg_list.remove(original_arg_list[random_num])
+
+print random_order_list
---
+++ stringReversal.py
@@ -0,0 +1,42 @@
+"""
+   The goal of this python script is to reverse the string(s) input that the
+   user enters and print the result
+
+   If the user enters one word then that word is reversed
+   Example:
+     python stringReversal.py Hello
+     'olleH'
+
+   If the user enters more than one word than the order the words are inputed
+   are reversed.
+   Example:
+     python stringReversal.py Hello World
+     'World Hello'
+"""
+
+import sys
+
+
+def reverse_word(word):
+    reversed_word = ""
+    for i in range(0, len(word)):
+        reversed_word += word[len(word) - 1 - i]
+    return reversed_word
+
+
+def reverse_list(sentence):
+    reversed_sentence = ""
+    for i in range(0, len(sentence)):
+        reversed_sentence += sentence[len(sentence) - 1 - i] + ' '
+    return reversed_sentence
+
+
+argument_list = list(sys.argv)
+# Remove the command input from argv list
+argument_list.pop(0)
+
+if len(argument_list) == 1:
+    print reverse_word(argument_list[0])
+else:
+    print reverse_list(argument_list)
+    # for i in argument_lis:
