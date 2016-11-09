2b33ff95297abf9cd55033c4b2b3eabd77b054cb
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-08 22:48:57 -0800

    added basic functctions for unique_words and frequency

--- word_frequency.py
+++ word_frequency.py
@@ -21,29 +21,32 @@ def histogram(file_name):
             histogram[word] += 1
         else:
             histogram[word] = 1
+    return histogram
 
-    print histogram['all']
-    print 'ima histogram.'
 
-'''
-A function that takes a histogram argument and returns the total count of
-unique words in the histogram. For example, when given the histogram
-for The Adventures of Sherlock Holmes, it returns the integer 8475.
-'''
-def unique_words():
-    return 1
+def unique_words(histogram):
+    '''
+    A function that takes a histogram argument and returns the total count of
+    unique words in the histogram. For example, when given the histogram
+    for The Adventures of Sherlock Holmes, it returns the integer 8475.
+    '''
+    return len(histogram)
 
-'''
-A function that takes a word and histogram argument and returns the number of
-times that word appears in a text. For example, when given the word "mystery"
-and the Holmes histogram, it will return the integer 20.
-'''
-def frequency():
-    return 1
+
+def frequency(histogram, word):
+    '''
+    A function that takes a word and histogram argument and returns the number of
+    times that word appears in a text. For example, when given the word "mystery"
+    and the Holmes histogram, it will return the integer 20.
+    '''
+    return histogram[word]
 
 
 def main():
     file_name = 'steve_jobs_speech.txt'
-    histogram(file_name)
+    histogram_data = histogram(file_name)
+    print unique_words(histogram_data)
+    print frequency(histogram_data, 'all')
+
 
 main()
