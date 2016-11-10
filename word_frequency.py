ef715e1d419d1cca2a57b20766b9a1612d3ceaef
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-09 23:08:51 -0800

    added basic rtreuly random word generator with weighted values based on occurances

--- word_frequency.py
+++ word_frequency.py
@@ -8,6 +8,43 @@ def return_random_word(histogram):
     return random_key[0]
 
 
+def get_token_count(histogram):
+    sum = 0
+    for key in histogram:
+        sum += histogram[key]
+    return sum
+
+
+def binary_search(histogram, histogram_keys, key_count, position):
+    get_total_token_count_to_point = 0
+    # if(histogram[histogram_keys[key_count/2] ==  )
+
+
+
+def return_weighted_random_word(histogram):
+    # Step 1: Get total count of all words in histogram
+    print histogram
+    token_count = get_token_count(histogram)
+    key_count = len(histogram.keys())
+    # Step 2: Generate random number between 0 and total count - 1
+    random_int = random.randint(0, token_count-1)
+    index = 0
+    list_of_keys = histogram.keys()
+    for i in range(0, len(histogram)):
+        index += histogram[list_of_keys[i]]
+        print index
+
+
+    print 'the random position is:', random_int
+    random_word = ''
+
+    # Here is where I can use a binary search?  Start by going to the half way
+    # of all dictionary keys...Then get sum from all the keys previously and
+    # see if > or < than the random value?
+
+
+
+
 def tuple_histogram(fime_name):
     # NOT WORKING
     data_file = open(file_name, 'r')
@@ -64,9 +101,11 @@ def frequency(histogram, word):
 
 
 def main():
-    file_name = 'steve_jobs_speech.txt'
+    file_name = 'small_text_sample.txt'
     histogram_data = histogram(file_name)
     print return_random_word(histogram_data)
+
+    return_weighted_random_word(histogram_data)
     # print unique_words(histogram_data)
     # print frequency(histogram_data, 'all')
 
