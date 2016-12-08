6ab3b9fb40ea378eb0029b103965721dea4eb55c
Alexander Dejeu <alexdejeu@gmail.com>
2016-12-07 23:36:18 -0800

    added the silicon Valley corpus and created a script to clean it using regex

--- benchmarked_histograms.py
+++ benchmarked_histograms.py
@@ -3,6 +3,7 @@ import random
 from operator import itemgetter
 import timeit
 
+
 class Dictogram(dict):
     def __init__(self, iterable=None):
         """Initialize this histogram as a new dict; update with given items"""
@@ -204,6 +205,7 @@ def test_binary_search(histogram, times):
 
     print sorted_results
 
+
 def test_weighted_random_word(histogram, times):
     results = dict()
     for i in range(0, times):
@@ -236,77 +238,77 @@ def test_weighted_random_word(histogram, times):
 
 def list_of_words(length):
     dict_words = '/usr/share/dict/words'
-    words_str  = open(dict_words, 'r').read()
-    all_words  = words_str.split("\n")
+    words_str = open(dict_words, 'r').read()
+    all_words = words_str.split("\n")
     return all_words[0:length]
 
-def main():
-    file_name = 'steve_jobs_speech.txt'
-    print '*********** GETTING DATA ***********'
-    print 'Start reading/parsing file'
+
+# file_name = 'steve_jobs_speech.txt'
+# print '*********** GETTING DATA ***********'
+# print 'Start reading/parsing file'
+#
+#
+# data_file = open(file_name, 'r')
+# words_list = data_file.read().split()
+# for word in words_list:
+#     word = word.decode('utf-8').lower().encode('utf-8')
+#
+# total_read_parse_time = 3
+# print 'Finished reading/parsing file in: ', total_read_parse_time, 'seconds'
+
+hundred_words = list_of_words(100)
+ten_thousand_words = list_of_words(10000)
+
+hundred_hgram = Dictogram(hundred_words)
+ten_thousand_hgram = Dictogram(ten_thousand_words)
+hundred_search = hundred_words[-1]
+ten_thousand_search = ten_thousand_words[-1]
+stmt = "ten_thousand_hgram.count('{}')".format(ten_thousand_search)
+setup = "from __main__ import ten_thousand_hgram"
+timer = timeit.Timer(stmt, setup=setup)
+
+iterations = 10000
+result = timer.timeit(number=iterations)
+print("count time for 100-word histogram: " + str(result))
+
+# '*********** CREATING DATA STRUCTURES ***********'
+# 'Start creating Dictogram'
+# dictogram = Dictogram(words_list)
+# 'Time to create Dictogram: ', 'seconds'
+#
+#
+# listogram = Listogram(words_list)
+# 'Time to create Listogram: ', 'seconds'
+#
+# listogram_sorted = Listogram(words_list)
+# listogram_sorted.sort_self_linear()
+# 'Time to create sorted Listogram: ', 'seconds'
+#
+# listogram_binary = Listogram(words_list)
+# listogram_binary.binary_self()
+# 'Time to create binary searchable Listogram: ', 'seconds'
+#
+#
+# 'Time to create HashTable: ', 'seconds'
+#
+#
+# '*********** TESTING TIME TO GET LENGTH ***********'
+# 'Time to get length of Dictogram: ', 'seconds'
+# 'Time to get length of Listogram: ', 'seconds'
+# 'Time to get length of sorted Listogram: ', 'seconds'
+# 'Time to get length of binary searchable Listogram: ', 'seconds'
+# 'Time to get length of HashTable', 'seconds'
 
 
-    data_file = open(file_name, 'r')
-    words_list = data_file.read().split()
-    for word in words_list:
-        word = word.decode('utf-8').lower().encode('utf-8')
 
-    total_read_parse_time = 3
-    print 'Finished reading/parsing file in: ', total_read_parse_time, 'seconds'
-
-    hundred_words = list_of_words(100)
-    ten_thousand_words = list_of_words(10000)
-
-    hundred_hgram = Dictogram(hundred_words)
-    ten_thousand_hgram = Dictogram(ten_thousand_words)
-    hundred_search = hundred_words[-1]
-    ten_thousand_search = ten_thousand_words[-1]
-    stmt = "count('{}', hundred_hgram)".format(hundred_search)
-    setup = "from __main__ import count, hundred_hgram"
-    timer = timeit.Timer(stmt, setup=setup)
-
-    iterations = 10000
-    result = timer.timeit(number=iterations)
-    print("count time for 100-word histogram: " + str(result))
-
-    '*********** CREATING DATA STRUCTURES ***********'
-    'Start creating Dictogram'
-    dictogram = Dictogram(words_list)
-    'Time to create Dictogram: ', 'seconds'
+# '*********** TESTING TIME TO GET RANDOM WEIGHTED WORD ***********'
+# 'In these tests the functions are run 10000 times and then the average is calculated'
+# 'Time to get random weighted word -> dictogram: '
+# # print weighted_random_word_tuple(histogram_data)
+# test_weighted_random_word(dictogram, 10000)
+# print unique_words(histogram_data)
+# print frequency(histogram_data, 'all')
 
 
-    listogram = Listogram(words_list)
-    'Time to create Listogram: ', 'seconds'
-
-    listogram_sorted = Listogram(words_list)
-    listogram_sorted.sort_self_linear()
-    'Time to create sorted Listogram: ', 'seconds'
-
-    listogram_binary = Listogram(words_list)
-    listogram_binary.binary_self()
-    'Time to create binary searchable Listogram: ', 'seconds'
-
-
-    'Time to create HashTable: ', 'seconds'
-
-
-    '*********** TESTING TIME TO GET LENGTH ***********'
-    'Time to get length of Dictogram: ', 'seconds'
-    'Time to get length of Listogram: ', 'seconds'
-    'Time to get length of sorted Listogram: ', 'seconds'
-    'Time to get length of binary searchable Listogram: ', 'seconds'
-    'Time to get length of HashTable', 'seconds'
-
-
-
-    '*********** TESTING TIME TO GET RANDOM WEIGHTED WORD ***********'
-    'In these tests the functions are run 10000 times and then the average is calculated'
-    'Time to get random weighted word -> dictogram: '
-    # print weighted_random_word_tuple(histogram_data)
-    test_weighted_random_word(dictogram, 10000)
-    # print unique_words(histogram_data)
-    # print frequency(histogram_data, 'all')
-
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
---
+++ cleanup.py
@@ -0,0 +1,17 @@
+import sys
+import re
+
+def main():
+    user_argument_count = len(sys.argv)
+    if user_argument_count == 1:
+        print 'Error: textfile not provided'
+    else:
+        data_file = open(sys.argv[1], 'r')
+        words_list = data_file.read()
+        print words_list
+
+        matches = re.findall("[A-z]+\'?[A-z]*|\$[0-9]*", words_list)
+        for match in matches:
+            print match
+
+main()
