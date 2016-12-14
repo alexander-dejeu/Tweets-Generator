b0d7f40edd615aa8406a5374931608f2ff327e7f
Alexander Dejeu <alexdejeu@gmail.com>
2016-12-14 14:32:40 -0800

    implemented nth order markov models

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 import cleanup
 from histograms import Dictogram
 import random
+from collections import deque
 # import word_frequency
 
 # print cleaned_file
@@ -42,6 +43,62 @@ cleaned_file = cleanup.clean_file(file_name)
 markov_model = make_markov_model(cleaned_file)
 print generate_random_sentence(15, markov_model)
 
+## Hello Alex. Hello John how are you
+## Hello, 1 --> ((Alex, 1) --> Hello),
+##            (((John,1) --> how))
+##           )
+##
+##
+## ( (HEAD, (#, DICTOGRAM))
+## DICTOGRAM has KEYS that are t (Hello , 1) --> None
+##
+
+
+def make_higher_order_markov_model(order, data):
+    markov_model = dict()
+
+    for i in range(0, len(data)-order):
+        # Create the window
+        window = tuple(data[i: i+order])
+        # Add to the dictionary
+        if window in markov_model:
+            # We have to just append to the existing histogram?
+            markov_model[window].update([data[i+order]])
+        else:
+            markov_model[window] = Dictogram([data[i+order]])
+
+    return markov_model
+
+
+def generate_random_sentence_n(length, markov_model):
+    current_window = generate_random_start(markov_model)
+    sentence = [current_window[0]]
+    # for i in range (0, len(current_window)):
+    #     sentence.append(current_window[i])
+
+    for i in range(0, length):
+        # print 'THE CURRENT WINDOW', current_window
+        current_dictogram = markov_model[current_window]
+        # print current_dictogram
+        random_weighted_word = current_dictogram.return_weighted_random_word()
+        current_window_deque = deque(current_window)
+        current_window_deque.popleft()
+        current_window_deque.append(random_weighted_word)
+
+        current_window = tuple(current_window_deque)
+        sentence.append(current_window[0])
+    sentence = list(sentence)
+    sentence[0] = sentence[0].capitalize()
+    return ' '.join(sentence) + '.'
+    return sentence
+
+
+file_name = 'SiliconValley.txt'
+cleaned_file = cleanup.clean_file(file_name)
+
+markov_model_nth = make_higher_order_markov_model(3, cleaned_file)
+# print markov_model_nth
+print generate_random_sentence_n(15, markov_model_nth)
 #
 # markov_window = dict()
 #
