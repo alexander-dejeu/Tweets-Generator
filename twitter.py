42c8d667e7d315caee5a4b30689b9fb97e4df68e
Alexander Dejeu <alexdejeu@gmail.com>
2016-12-26 23:15:33 -0800

    added basic twitter environment variables

---
+++ twitter.py
@@ -0,0 +1,9 @@
+import os
+import dotenv
+
+dotenv.load_dotenv('.env')
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
